a0b04e1a1d9b078732b46e749309ff7ed953ac0a
Madison <madison.c.quiroga-1@ou.edu>
2020-10-21 16:32:50 -0500

    Make a stack

---
+++ SuperStack.java
@@ -0,0 +1,27 @@
+
+public class SuperStack {
+
+	private T[] items;
+	private int top;
+	
+	public SuperStack(int size){
+		items = (T[])new Object(size);
+	}
+	
+	
+	public boolean isEmpty() {
+	return true;
+}
+
+	public boolean isFull() {
+	return true;
+}
+
+	public boolean push() {
+	return true;
+}
+
+	public boolean pop() {
+	return true;
+}
+}
